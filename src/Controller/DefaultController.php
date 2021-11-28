c7af2fa07455da4619d63725fbc7b7e69decf05c
Drx85 <cedric@deperne.fr>
2021-11-28 17:51:38 +0100

    Suzuki scrapper done + refactor

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,31 +2,16 @@
 
 namespace App\Controller;
 
+use App\Service\Scrapper\Suzuki;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Goutte\Client;
+
 
 class DefaultController extends AbstractController
 {
 	#[Route('/', name: 'test-suzuki-scrapping')]
-	public function index()
+	public function index(Suzuki $suzuki)
 	{
-		$client = new Client();
-		$crawler = $client->request('GET', 'https://www.suzuki-moto.com');
-		$links = $crawler->filter('div.block-category.model > a')->each(function ($node) {
-			$link = $node->attr('href');
-			if (str_contains($link, '/scooters')) return;
-			return $link;
-		});
-
-		$motos = array();
-		foreach (array_filter($links) as $link) {
-			$crawler = $client->request('GET', 'https://www.suzuki-moto.com/' . $link);
-			$name = $crawler->filter('.essentiel.desktop')->text();
-			$caracteristics = $crawler->filter('.tab-content.tab-content-1')->text();
-			$moto = $name . ' - ' . $caracteristics;
-			$motos[] = $moto;
-		}
-		dd($motos);
+	
 	}
 }
---
+++ src/Service/Scrapper/Suzuki.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Service\Scrapper;
+
+use Goutte\Client;
+
+class Suzuki
+{
+	public function __construct()
+	{
+		$client = new Client();
+		$crawler = $client->request('GET', 'https://www.suzuki-moto.com');
+		$links = $crawler->filter('div.block-category.model > a')->each(function ($node) {
+			$link = $node->attr('href');
+			if (str_contains($link, '/scooters')) return;
+			return $link;
+		});
+		
+		foreach (array_filter($links) as $link) {
+			$crawler = $client->request('GET', 'https://www.suzuki-moto.com/' . $link);
+			$name = $crawler->filter('.essentiel.desktop')->text();
+			$motos[$name]['type'] = $this->getType($link);
+			
+			$characteristicNames[$name] = $crawler->filter('.tab-content.tab-content-1 .left')->each(function ($node) {
+				return $node->text();
+			});
+			
+			$motos[$name]['characteristics'] = $crawler->filter('.tab-content.tab-content-1 .right')->each(function ($node) {
+				return $node->text();
+			});
+		}
+		$motos = $this->mergeArrays($motos, $characteristicNames);
+		dd($motos);
+	}
+	
+	/**
+	 * Return the type of moto, depending on what contains the given URL.
+	 * 'special' is default value.
+	 *
+	 * @param string $link
+	 *
+	 * @return string
+	 */
+	private function getType(string $link): string
+	{
+		return match (true) {
+			str_contains($link, 'trails-routiers') => 'trail',
+			str_contains($link, 'sportives') => 'sporty',
+			str_contains($link, 'roadsters') => 'roadster',
+			str_contains($link, 'motos-125-cm3') => '125',
+			str_contains($link, 'tout-terrain') => 'offroad',
+			default => 'special',
+		};
+	}
+	
+	/**
+	 * Add to the array which contains moto characteristics the name of these characteristics, thanks to another array
+	 *
+	 * @param array $motos
+	 * @param array $characteristicNames
+	 *
+	 * @return array
+	 */
+	private function mergeArrays(array $motos, array $characteristicNames): array
+	{
+		$keys = array_keys($motos);
+		for ($i = 0; $i < count($motos); $i++) {
+			foreach ($motos[$keys[$i]]['characteristics'] as $ignored) {
+				$c = 0;
+				foreach ($keys as $value) {
+					$motos[$keys[$c]]['characteristics'] = array_combine($characteristicNames[$value], $motos[$keys[$c]]['characteristics']);
+					$c++;
+				}
+			}
+		}
+		return $motos;
+	}
+}
